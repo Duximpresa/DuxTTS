40bc2c8a5017e5f3eea31940a846150097ec55a2
Duximpresa <yccnaruto@Hotmail.com>
2022-07-13 17:52:01 +0800

    增加一些代码

--- speech2text.py
+++ speech2text.py
@@ -1,11 +1,27 @@
 import azure.cognitiveservices.speech as speechsdk
 
-def from_file():
-    speech_config = speechsdk.SpeechConfig(subscription="<paste-your-speech-key-here>", region="<paste-your-speech-location/region-here>")
-    audio_input = speechsdk.AudioConfig(filename="your_file_name.wav")
+
+# 输入的参数
+
+language_list = ["zh-CN", "en-US"]
+filename = "kepu-02.wav" # 文件路径
+language = language_list[0]
+
+def from_file(filename, language):
+    speech_config = speechsdk.SpeechConfig(subscription="8b7335e4c1cf4708a48453f878a6c802", region="southeastasia")
+    audio_input = speechsdk.AudioConfig(filename=filename)
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+
+    speech_config.speech_recognition_language=language
 
     result = speech_recognizer.recognize_once_async().get()
     print(result.text)
+    return result.text
+# print(language)
+resultText = from_file(filename, language)
 
-from_file()
+with open("b.txt", mode="w", encoding="utf-8") as f:
+    f.write(resultText)
+    f.close
+
+
